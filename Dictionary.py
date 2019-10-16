0afc81a05adb975bdffad28c98cabe904b7d7a64
rneil06 <56591303+rneil06@users.noreply.github.com>
2019-10-16 13:50:39 +0100

    Create Dictionary.py

---
+++ Dictionary.py
@@ -0,0 +1,26 @@
+import json
+from difflib import get_close_matches
+collection = json.load(open("collection.json"))
+def lookup(word):
+    word = word.lower()
+    if word in collection :
+        return collection[word]
+    elif len(get_close_matches(word, collection.keys())) > 0:
+        yn = input("Are you looking for the word %s ? Enter Y for Yes or N for No:  " %get_close_matches(word, collection.keys())[0])
+        if yn == 'Y':
+            return collection[get_close_matches(word, collection.keys())[0]]
+        elif yn == 'N':
+              return "Word not found. Please try again." 
+
+        else:
+            return "Please enter either Y or N"
+    else:
+        return "No word was entered"
+print("Welcome to the English Dictionary. ")
+word = input("Enter word: ")
+output = lookup(word)
+if type(output) == list:
+    for item in output:
+        print(item)
+
+else: print(output)
